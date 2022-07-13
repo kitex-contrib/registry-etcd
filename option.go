ffb806910c4546c7015b8def7aceba15c4b56e71
zmdaily404 <zmdaily404@gmail.com>
2022-07-13 22:45:30 +0800

    fix: tls config and add license

--- option.go
+++ option.go
@@ -1,9 +1,27 @@
+// Copyright 2021 CloudWeGo Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package etcd
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"io/ioutil"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"github.com/cloudwego/kitex/pkg/klog"
 )
 
 type Option func(cfg *clientv3.Config)
@@ -12,6 +30,7 @@ func WithTlsOpt(certFile, keyFile, caFile string) Option {
 	return func(cfg *clientv3.Config) {
 		tlsCfg, err := newTLSConfig(certFile, keyFile, caFile, "")
 		if err != nil {
+			klog.Errorf("tls failed with err: %v , skipping tls.", err)
 			tlsCfg = nil
 		}
 		cfg.TLS = tlsCfg
@@ -26,22 +45,22 @@ func WithAuthOpt(username, password string) Option {
 }
 
 func newTLSConfig(certFile, keyFile, caFile, serverName string) (*tls.Config, error) {
-	var (
-		tlsCfg *tls.Config
-		err    error
-	)
-
-	if certFile != "" || keyFile != "" || caFile != "" || serverName != "" {
-		cfgtls := &TLSInfo{
-			CertFile:      certFile,
-			KeyFile:       keyFile,
-			TrustedCAFile: caFile,
-			ServerName:    serverName,
-		}
-		if tlsCfg, err = cfgtls.ClientConfig(); err != nil {
-			return nil, err
-		}
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return &tls.Config{}, err
 	}
-
-	return tlsCfg, nil
+	caCert, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		return &tls.Config{}, err
+	}
+	caCertPool := x509.NewCertPool()
+	successful := caCertPool.AppendCertsFromPEM(caCert)
+	if !successful {
+		return &tls.Config{}, errors.New("failed to parse ca certificate as PEM encoded content")
+	}
+	cfg := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs: caCertPool,
+	}
+	return cfg, nil
 }
